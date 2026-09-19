43adf80f4a16a848f1ca7ea77fc608585ddb6345
chmodmasx <daninicob@gmail.com>
2026-09-18 21:29:04 -0300

    fix(ci): honor upstream QuickCharts target exports

--- packages/kde/kquickcharts/consumer/main.cpp
+++ packages/kde/kquickcharts/consumer/main.cpp
@@ -1,12 +1,10 @@
 #include <QCoreApplication>
 
 void qml_register_types_org_kde_quickcharts();
-void qml_register_types_org_kde_quickcharts_controls();
 
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
     qml_register_types_org_kde_quickcharts();
-    qml_register_types_org_kde_quickcharts_controls();
     return 0;
 }
